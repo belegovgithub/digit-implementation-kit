47f04ffb4c2b5f5b1a3a06f13174401f3f1cc887
nvcs057 <nvcs057@gmail.com>
2020-10-29 10:14:46 +0530

    curl call for trade category

--- processing/process_tradeCategory.py
+++ processing/process_tradeCategory.py
@@ -5,26 +5,86 @@ from pathlib import Path
 from config import config
 from common import *
 
-dfs = open_excel_file(config.BANK_WORKBOOK)
-COL_INDEX = 2
 
+dfs = open_excel_file(config.TRADE_TYPE_WORKBOOK)
+tradeTypeCodes = get_sheet(dfs, config.SHEET_TRADETYPE_LIST)
+tradeTypeCodes = tradeTypeCodes.astype(str)
 
-def bank():
-    bank  = get_sheet(dfs, config.SHEET_BANK_BRANCH)
-    bank = bank.astype(str)
-    INDEX_BANK_NAME = 10
-    INDEX_BANK_CODE = 11
-    bank_name = fix_value(bank.iloc[INDEX_BANK_NAME][COL_INDEX])
-    bank_code = fix_value(bank.iloc[INDEX_BANK_CODE][COL_INDEX])
+INDEX_DOC_NAME = 1
+INDEX_APPL_TYPE_1 = 2
+INDEX_APPL_TYPE_2 = 3
+INDEX_MANDATORY_FLAG = 4
+COL_INDEX=1
 
-    bank_data = []
-    bank_data.append({
-                        "code": bank_code.strip(),
-                        "name": bank_name.strip(),
-                        "active": True,
-                        "type": "I",
-                        "tenantId": config.TENANT_ID
-                    })
+INDEX_TRADE_TYPE = 1
+INDEX_TRADE_CAT = 2
+INDEX_TRADE_NEW_FEE = 6
+INDEX_TRADE_RENEW_FEE = 8
+INDEX_TRADE_UOM = 7
+COL_INDEX=1
+bank_data = []
+tradeType_final=[]
+docCodes_new=[]
+docCodes_renew=[]
+i=1
+j=1
+ 
+  
+tradeType_uom = ""
+tradeType_trade = []
+uom = ""
+
+  
+for j in range(1,len(tradeTypeCodes)) :
+    if tradeTypeCodes.iloc[j, INDEX_TRADE_NEW_FEE] is not None:
+      tradeType_category = "TRADE"
+    if config.TRADETYPE_EATING in tradeTypeCodes.iloc[j, INDEX_TRADE_CAT].upper():
+      tradeType_category = tradeType_category+"."+config.TRADETYPE_EATING
+    elif config.TRADETYPE_MEDICAL in tradeTypeCodes.iloc[j, INDEX_TRADE_CAT].upper():
+      tradeType_category = tradeType_category+"."+config.TRADETYPE_MEDICAL 
+    elif config.TRADETYPE_VETERINARY in tradeTypeCodes.iloc[j, INDEX_TRADE_CAT].upper():
+      tradeType_category = tradeType_category+"."+config.TRADETYPE_VETERINARY
+    elif config.TRADETYPE_DANGEROUS in tradeTypeCodes.iloc[j, INDEX_TRADE_CAT].upper():
+      tradeType_category = tradeType_category+"."+config.TRADETYPE_DANGEROUS
+    elif config.TRADETYPE_GENERAL in tradeTypeCodes.iloc[j, INDEX_TRADE_CAT].upper():
+      tradeType_category = tradeType_category+"."+config.TRADETYPE_GENERAL 
+    elif config.TRADETYPE_PRIVATE in tradeTypeCodes.iloc[j, INDEX_TRADE_CAT].upper():
+      tradeType_category = tradeType_category+"."+config.TRADETYPE_PRIVATE
+    tradeType_category = tradeType_category+"."+tradeTypeCodes.iloc[j, INDEX_TRADE_TYPE]
+    if tradeTypeCodes.iloc[j, INDEX_TRADE_UOM] == "FIXED":
+      tradeType_uom = "FLAT"
+      uom = 'null'
+    else :
+      tradeType_uom = "RATE"
+      if 
+
+    
+      docCodes_new=[]
+      docCodes_renew=[]
+    tradeType_trade.append({ "tenantId": "pb.delhi",
+                             "licenseType": "PERMANENT",
+                             "structureType": "IMMOVABLE.PUCCA",
+                             "tradeType": tradeType_category,
+                             "accessoryCategory": null,
+                              "type": tradeType_uom,
+                             "applicationDocument" :[
+                              {
+                              "applicationType": "NEW",
+                               "documentList": docCodes_new
+                              },
+                              {
+                              "applicationType": "RENEWAL",
+                               "documentList": docCodes_renew
+                              }
+                            ],
+                            "active": "true",
+                            "type": "TL",
+                            "validityPeriod": "null",
+                            "verificationDocument": []
+                            })
+  
+  
+print(tradeType_trade)
 
     data = {
         "RequestInfo": {
