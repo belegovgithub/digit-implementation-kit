eae4c439214f9ab55089cf0feee8bc576d9a9f4a
vipin <vipinpublicdomain@gmail.com>
2021-02-06 19:03:53 +0530

    Update WS_DocGen.py

--- WS_DocGen.py
+++ WS_DocGen.py
@@ -4,178 +4,219 @@ import io
 import os 
 import sys
 import pandas as pd
+
+def getTemplate() :
+    docList =[
+    {
+      "code": "OWNER.IDENTITYPROOF",
+      "documentType": "OWNER",
+      "required": True,
+      "active": True,
+      "hasDropdown": True,
+      "dropdownData": [
+        {
+          "code": "OWNER.IDENTITYPROOF.AADHAAR",
+          "active": True
+        },
+        {
+          "code": "OWNER.IDENTITYPROOF.VOTERID",
+          "active": True
+        },
+        {
+          "code": "OWNER.IDENTITYPROOF.DRIVING",
+          "active": True
+        },
+        {
+          "code": "OWNER.IDENTITYPROOF.PAN",
+          "active": True
+        },
+        {
+          "code": "OWNER.IDENTITYPROOF.PASSPORT",
+          "active": True
+        }
+      ],
+      "description": "OWNER.ADDRESSPROOF.IDENTITYPROOF_DESCRIPTION"
+    },
+    {
+      "code": "OWNER.ADDRESSPROOF",
+      "documentType": "OWNER",
+      "required": True,
+      "active": True,
+      "hasDropdown": True,
+      "dropdownData": [
+        {
+          "code": "OWNER.ADDRESSPROOF.ELECTRICITYBILL",
+          "active": True
+        },
+        {
+          "code": "OWNER.ADDRESSPROOF.DL",
+          "active": True
+        },
+        {
+          "code": "OWNER.ADDRESSPROOF.VOTERID",
+          "active": True
+        },
+        {
+          "code": "OWNER.ADDRESSPROOF.AADHAAR",
+          "active": True
+        },
+        {
+          "code": "OWNER.ADDRESSPROOF.PAN",
+          "active": True
+        },
+        {
+          "code": "OWNER.ADDRESSPROOF.PASSPORT",
+          "active": True
+        }
+      ],
+      "description": "OWNER.ADDRESSPROOF.ADDRESSPROOF_DESCRIPTION"
+    },
+    {
+      "code": "PROPERTY_TAX_RECIEPT",
+      "documentType": "PROPERTY_TAX_RECIEPT",
+      "active": True,
+      "required": True,
+      "hasDropdown": True,
+      "dropdownData": [
+        {
+          "code": "PROPERTY_TAX_RECIEPT",
+          "active": True
+        }
+      ],
+      "description": "PROPERTY_TAX_RECIEPT_DESCRIPTION"
+    },
+    {
+      "code": "SELFDECLERATION",
+      "documentType": "SELFDECLERATION",
+      "active": True,
+      "required": True,
+      "hasDropdown": True,
+      "dropdownData": [
+        {
+          "code": "SELFDECLERATION",
+          "active": True
+        }
+      ],
+      "description": "SELFDECLERATION_DESCRIPTION"
+    } 
+    
+  ]
+    return docList
+
+
+def getValue(df, row,colName,defValue="") :
+    if not pd.isna(row[df.columns.get_loc(colName)] ) : 
+        return str(row[df.columns.get_loc(colName)]).strip() 
+    else : 
+        return defValue if defValue is not None else row[df.columns.get_loc(colName)] 
 def main():
-    Flag =False
-    docDict = loadDocDict()
     tenantMapping={}
+    cbDocData ={}
+    localizationData =[]
     with io.open(config.TENANT_JSON, encoding="utf-8") as f:
         cb_module_data = json.load(f)
     for found_index, module in enumerate(cb_module_data["tenants"]):
         tenantMapping[module["description"].lower()]=module["code"]
-    print(tenantMapping)
-    #for root, dirs, files in os.walk(r"D:\CBData\Verified CB Data\CC", topdown=True):
-    for root, dirs, files in os.walk(r"D:\Temp\SC", topdown=True):
-        for name in dirs:
-            #print (os.path.join(root, name))
-            subfolder = os.path.join(root, name)
-            ws_doc_file =os.path.join(root, name,"Document Master for Water connection.xlsx")
-            if os.path.exists(ws_doc_file) :
-                city = subfolder.replace(r"D:\Temp\SC\CB ","" ).strip().lower()
-                if city not in tenantMapping:
-                    print("Not In city",city)
-                    continue
-                config.CITY_NAME = city
-                config.TENANT_ID = tenantMapping[city]
-                callWS_Doc(ws_doc_file, docDict)
-                Flag=False
-        if Flag : 
-            break
-
-
-def callWS_Doc(ws_doc_file, docDict):
-    docCode_data = [{
-                        "code": "OWNER.IDENTITYPROOF",
-                        "documentType": "OWNER",
-                        "required": True,
-                        "active": True,
-                        "hasDropdown": True,
-                        "dropdownData": [
-                            {
-                            "code": "OWNER.IDENTITYPROOF.AADHAAR",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.IDENTITYPROOF.VOTERID",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.IDENTITYPROOF.DRIVING",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.IDENTITYPROOF.PAN",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.IDENTITYPROOF.PASSPORT",
-                            "active": True
-                            }
-                        ],
-                        "description": "OWNER.ADDRESSPROOF.IDENTITYPROOF_DESCRIPTION"
-                        },
-                        {
-                        "code": "OWNER.ADDRESSPROOF",
-                        "documentType": "OWNER",
-                        "required": True,
-                        "active": True,
-                        "hasDropdown": True,
-                        "dropdownData": [
-                            {
-                            "code": "OWNER.ADDRESSPROOF.ELECTRICITYBILL",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.ADDRESSPROOF.DL",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.ADDRESSPROOF.VOTERID",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.ADDRESSPROOF.AADHAAR",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.ADDRESSPROOF.PAN",
-                            "active": True
-                            },
-                            {
-                            "code": "OWNER.ADDRESSPROOF.PASSPORT",
-                            "active": True
-                            }
-                        ],
-                        "description": "OWNER.ADDRESSPROOF.ADDRESSPROOF_DESCRIPTION"
-                        },
-                        {
-                        "code": "PROPERTY_TAX_RECIEPT",
-                        "documentType": "PROPERTY_TAX_RECIEPT",
-                        "active": True,
-                        "required": True,
-                        "hasDropdown": True,
-                        "dropdownData": [
-                            {
-                            "code": "PROPERTY_TAX_RECIEPT",
-                            "active": True
-                            }
-                        ],
-                        "description": "PROPERTY_TAX_RECIEPT_DESCRIPTION"
-                        },
-                        {
-                        "code": "SELF_DECLERATION",
-                        "documentType": "SELF_DECLERATION",
-                        "active": True,
-                        "required": True,
-                        "hasDropdown": True,
-                        "dropdownData": [
-                            {
-                            "code": "SELF_DECLERATION",
-                            "active": True
-                            }
-                        ],
-                        "description": "SELF_DECLERATION_DESCRIPTION"
-                        }]
-    dfs = open_excel_file(ws_doc_file)
-    docNames = get_sheet(dfs, "Document Details")
-    docNames = docNames.astype(str)
-    row = docNames.iloc[0]
-    docCodes = []
-    for i in range(4,6):
-        if not pd.isna(row[i]) or row[i].strip() != 'nan':
-            docCodes.append(docDict[row[i]])
-    json.dump(docCodes, sys.stdout, indent=2)
-    return
-    for i in range(len(docCodes)):
-        docCode_data.append({
-                            "code": docCodes[i],
-                            "documentType": docCodes[i],
-                            "active": True,
-                            "required": True,
-                            "hasDropdown": True,
-                            "dropdownData": [
-                                {
-                                "code": docCodes[i],
-                                "active": True
-                                }
-                            ],
-                            "description": docCodes[i] + "_DESCRIPTION"
-                            })
-    final_data = {
-        "tenantId": config.TENANT_ID,
-        "moduleName": "ws-services-masters",
-        "Documents": [
-            docCode_data
-        ]
-    }
-
-    json.dump(final_data, sys.stdout, indent=2)
-    cityname = config.TENANT_ID.lower()[3:]
-    ws_path = config.MDMS_LOCATION / cityname / "ws"
-    os.makedirs(ws_path, exist_ok=True)       
-
-    with open(os.path.join(ws_path , "DocumentType.json"), "w") as f:
-        json.dump(final_data, f, indent=2)
-
+        cbDocData[module["description"].lower()]=getTemplate()
     
+    dfs = open_excel_file('D:\HRMS_Data\DocsMapping&Clarifications_1.xlsx')
+    df = get_sheet(dfs, "Sheet2")
+    for ind in df.index: 
+        row =df.iloc[ind]
+        cbName = getValue(df,row,"CB" ,None ).lower()
+        if cbName not in cbDocData : 
+            print("CB description not found ",cbName)
+            continue
+        cbDocs =cbDocData[cbName]
+        
+        docCategory = getValue(df,row,"Doc Category" ,None ) 
+        docDesc = getValue(df,row,"Doc Description " ,"" ) 
+        mandatoryFld = getValue(df,row,"M_OR_N" ,True ) 
+        dropDownCode = getValue(df,row,"DOC_MAPPING" ,None ) 
+        dropDownText = getValue(df,row,"Additional Docs" ,"" ) 
+        docList  = list(filter(lambda doc: doc["code"]==docCategory, cbDocs))
+        if docCategory is None or dropDownCode is None : 
+            print("Code is none for cb ",cbName)
+        if len(docList) == 0 : 
+            ele ={
+                    "code": docCategory,
+                    "documentType": docCategory,
+                    "active":True,
+                    "required": False,
+                    "hasDropdown": True,
+                    "dropdownData": [
+                        {
+                        "code": dropDownCode,
+                        "active": True
+                        }
+                    ],
+                    "description": str(docCategory) + "_DESCRIPTION"
+                } 
+            cbDocs.append(ele)
+        else :
+            ele ={
+                        "code": dropDownCode,
+                        "active": True
+                  }
+            docList[0]["dropdownData"].append(ele)
+        if len(list(filter(lambda doc: doc["code"]==docCategory, localizationData))) ==0 : 
+            localData ={
+                "locale": "en_IN",
+                "code": docCategory,
+                "message": docDesc,
+                "module": "rainmaker-ws"
+            }
+            localizationData.append(localData)
+            localData ={
+                "locale": "en_IN",
+                "code": docCategory+"_DESCRIPTION",
+                "message": docDesc,
+                "module": "rainmaker-ws"
+            }
+            localizationData.append(localData)
+        if len(list(filter(lambda doc: doc["code"]==dropDownCode, localizationData))) ==0 : 
+            localData ={
+                "locale": "en_IN",
+                "code": dropDownCode,
+                "message": dropDownText,
+                "module": "rainmaker-ws"
+            }
+            localizationData.append(localData)
+
+         
+        
+
+    print(json.dumps(cbDocData))
+    with io.open(os.path.join(r"D:\HRMS_Data","localization.json"), mode="w", encoding="utf-8") as f:
+        json.dump(localizationData, f, indent=2,  ensure_ascii=False, cls=DateTimeEncoder)   
+    with io.open(os.path.join(r"D:\HRMS_Data","cbData.json"), mode="w", encoding="utf-8") as f:
+        json.dump(cbDocData, f, indent=2,  ensure_ascii=False, cls=DateTimeEncoder)       
+    for ele in cbDocData : 
+        docList=cbDocData[ele]
+        templateToPush ={
+            "tenantId": tenantMapping[ele],
+            "moduleName": "ws-services-masters",
+            "Documents": docList
+        }
+        modifyTemplateToPush ={
+            "tenantId": tenantMapping[ele],
+            "moduleName": "ws-services-masters",
+            "ModifyConnectionDocuments": docList
+        }
+        mCollect_path = config.MDMS_LOCATION / ele / "ws-services-masters"
+        os.makedirs(mCollect_path, exist_ok=True)    
+        with io.open(os.path.join(mCollect_path,"Documents.json"), mode="w", encoding="utf-8") as f:
+            json.dump(templateToPush, f, indent=2,  ensure_ascii=False, cls=DateTimeEncoder)   
+        with io.open(os.path.join(mCollect_path,"ModifyConnectionDocuments.json"), mode="w", encoding="utf-8") as f:
+            json.dump(modifyTemplateToPush, f, indent=2,  ensure_ascii=False, cls=DateTimeEncoder)   
+       
     
-def loadDocDict():
-  docDict = {
-    "Proof Of Identity":"OWNER.IDENTITYPROOF",
-    "Proof Of Address":"OWNER.ADDRESSPROOF",
-    "Property Tax Receipt":"PROPERTY_TAX_RECIEPT",
-    "Self Decleration":"SELF_DECLERATION"
-  }   
-  return docDict 
+        
+
+
+
+
+  
 
 if __name__ == "__main__":
     main()
