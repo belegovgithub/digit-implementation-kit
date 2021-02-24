6c7da3c8dd6a2c1638d181247060b370ecc1ac96
Vidyabhushan300 <vidyaisbhushan@gmail.com>
2021-02-24 09:37:01 +0530

    generate property template

---
+++ GeneratePropertyTemplate.py
@@ -0,0 +1,90 @@
+import requests
+import csv
+from common import *
+from config import config ,load_employee_creation_config
+from common import superuser_login
+from config import config
+import io
+import os
+import numpy
+import pandas as pd
+import openpyxl
+from datetime import datetime, timedelta
+from math import isnan
+
+# import shutil
+# import xlrd
+# import requests
+# import xlwt
+# from xlwt import Workbook
+#import xlsxwriter
+
+def main():
+    Flag =False
+    tenantMapping={}
+    post_data_resp_list=[]
+
+    with io.open(config.TENANT_JSON, encoding="utf-8") as f:
+        cb_module_data = json.load(f)
+        for found_index, module in enumerate(cb_module_data["tenants"]):
+            if module["city"]["ulbGrade"]=="ST":
+                continue
+            data = []
+            
+            tenant_id=module["code"]
+            cityname = tenant_id.lower()[3:]
+            #cityname = "ahmednagar"
+            boundary_path = os.path.join(config.MDMS_LOCATION ,  cityname , "egov-location")
+            template_path = os.path.join(config.LOG_PATH ,  cityname )
+            print(cityname)
+            
+            if os.path.isfile(os.path.join(boundary_path , "boundary-data.json")):
+                with open(os.path.join(boundary_path , "boundary-data.json")) as f:
+                    existing_boundary_data = json.load(f)
+            found = False
+            for tenantboundary in existing_boundary_data["TenantBoundary"]:                
+                for tenant_boundary in tenantboundary["boundary"]["children"]:
+                    for t1 in tenant_boundary["children"]:
+                        for t2 in t1["children"]:
+                            data.append([t2["name"],t2["code"]])
+                            #print(json.dumps(t1, indent = 2))
+            df = pd.DataFrame(data,columns=['Locality Name','Code'])
+            os.makedirs(template_path, exist_ok=True)              
+
+            # #df.to_excel(os.path.join(template_path,'Template for Existing Property Detail.xlsx'),index=False)
+            #Open existing excel file
+            templateFile = os.path.join(template_path,'Template for Existing Property Detail.xlsx')
+            workbook1 = openpyxl.load_workbook(r'D:\eGov\Data\WS\Template\Template for Existing Property Detail.xlsx')         
+            writer = pd.ExcelWriter(templateFile, engine='openpyxl')   
+            writer.book = workbook1          
+            #Add dataframe to excel file 
+            df.to_excel(writer,sheet_name="Master_Locality/Mohalla",index=False,engine='openpyxl')                
+            writer.save()
+            writer.close()
+            #return
+            # shutil.copy(r'D:\eGov\Data\WS\Template\Template for Existing Property Detail.xlsx',os.path.join(template_path,'Template for Existing Property Detail.xlsx'))
+            # filePath =os.path.join(template_path,'Template for Existing Property Detail.xlsx')
+            # workbook = xlsxwriter.Workbook(filePath)
+            # worksheet = workbook.add_worksheet()
+
+            # worksheet.write(0, 0, 1234)     # Writes an int
+            # worksheet.write(1, 0, 1234.56)  # Writes a float
+            # worksheet.write(2, 0, 'Hello')  # Writes a string
+            # worksheet.write(3, 0, None)     # Writes None
+            # worksheet.write(4, 0, True)     # Writes a bool
+
+            # workbook.close()
+
+            # wb=openpyxl.load_workbook(os.path.join(template_path,'Template for Existing Property Detail.xlsx'), keep_vba = True)
+            # source=wb.get_sheet_by_name('Locality')
+            # # load demo.xlsx 
+            # # get Sheet
+            # # copy sheet
+            # target=wb.copy_worksheet(source)
+            # # save workbook
+            # wb.save(os.path.join(template_path,'Template for Existing Property Detail.xlsx'))
+        print("Done")    
+
+
+if __name__ == "__main__":
+    main()
--- Migration_Localization.py
+++ Migration_Localization.py
@@ -6,24 +6,26 @@ import os
 import numpy
 
 recv_host = "https://13.71.65.215.nip.io"
-send_host = "https://demo.echhawani.gov.in"
-recv_auth = "9804cdcf-d017-4cba-bf14-dd9c7185f6be"
-send_auth = "4ffbd256-8e0d-43f2-a0f5-11be9640c98c"
+#send_host = "https://demo.echhawani.gov.in"
+send_host = "https://echhawani.gov.in"
+recv_auth = "f5e4ba53-3d15-46f4-8bd5-09d74b34406a"
+#send_auth = "8b5bcc37-bdc5-47bd-a47f-0a879b4333b8"
+send_auth = "247a0a8c-d58e-4610-b4cb-3a8b9a28cc0d"
 tenantId = "pb"
 
 def main():
     auth_token = superuser_login()["access_token"]
-    modules = ["rainmaker-common","rainmaker-ws","rainmaker-pt","rainmaker-pdf","rainmaker-commonpay"]
+    modules = ["rainmaker-common","rainmaker-ws","rainmaker-pt"]
     # modules = ["rainmaker-common"]
     locales = ["en_IN","hi_IN"]
     # locales = ["en_IN"]
     for module in modules:
         for locale in locales:
             data = search_localization(recv_auth,module,locale)["messages"]   
-            with io.open(os.path.join(r"D:\Temp","localization.json"), mode="w", encoding="utf-8") as f:
-                json.dump(data, f, indent=2,  ensure_ascii=False, cls=DateTimeEncoder)         
+            # with io.open(os.path.join(r"D:\Temp","localization.json"), mode="w", encoding="utf-8") as f:
+            #     json.dump(data, f, indent=2,  ensure_ascii=False, cls=DateTimeEncoder)         
             upsert_localization(send_auth, data)
-    print("Migration Completed")
+            print(module, " ", locale)
 
 
 
@@ -45,8 +47,7 @@ def upsert_localization(auth_token, data):
         "messages": data
     }
 
-    body["RequestInfo"]["authToken"] = auth_token
-    
+    body["RequestInfo"]["authToken"] = auth_token    
     requests.post(url=send_host + '/localization/messages/v1/_upsert', json=body)
 
 
